7bb834b0e62b4df84b4c841e3054a89e7271fabe
Ramon Marron <ramonmarronr@gmail.com>
2024-02-14 23:34:46 -0400

    SELECCION DE MUSCULOS
    vincula a nivel de base de datos que musculos corresponden al ejercicio

---
+++ AplicativoDeportivoWeb/Migrations/20240214163031_MuscleToExercise.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AplicativoDeportivoWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class MuscleToExercise : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Muscle",
+                table: "Exercises",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Muscle",
+                table: "Exercises");
+        }
+    }
+}
--- AplicativoDeportivoWeb/Repository.Postgres/Exercises.cs
+++ AplicativoDeportivoWeb/Repository.Postgres/Exercises.cs
@@ -7,5 +7,5 @@ public class Exercises : KeyEntity<int>
     [StringLength(100)]
     public string Name { get; set; }
 
-
+    public string Muscle { get; set; } 
 }
